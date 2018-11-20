ea307277d9085dea962cae4d4aa057dd2f5e2773
2017440081 <ogaoga5959@gmail.com>
2018-11-20 14:38:14 +0900

    '2017440081과제'

--- 20181113selectionsort/selectionsort.cpp
+++ 20181113selectionsort/selectionsort.cpp
@@ -3,7 +3,7 @@
 	void printerArray(int* arr, int len) {  //앞으로 배열을 함수에 전달할 경우 이 문법을 주로 사용한다.
 	for(int index = 0; index < len; index++) {
 	
-		printf("array[%d] = %d \n", index, arr[index]); // index를 변수를 집어 넣을 수 있다.
+	       printf("array[%d] = %d \n", index, arr[index]); // index를 변수를 집어 넣을 수 있다.
 
 	}
 	}
@@ -12,14 +12,14 @@
 	//함수: findMinIndex()
 	//입력: 배열(배열이름, 배열길이)
 	//출력: 최소값의 인덱스
-	int findMinIndex(int* arr, int length) {
-	    int minIndex=0;
-		
+	int findMinIndex(int* arr, int length, int start) {
+		int minIndex= start;
+	
 //		다음시간 퀴즈!!
-		for(int count=1; count<length; count++) 
-		if( minIndex > arr[count])
-			minIndex= count;
-		
+		for(int i=start + 1; i<length; i++) {
+		if( arr[minIndex] > arr[i])
+	minIndex= i;
+	}
 
 		return minIndex;
 	
@@ -30,22 +30,21 @@
 	//함수: findMin()
 	//입력: 배열(배열이름, 배열길이)
 	//출력: 배열 최소값
-	int findMin(int* arr, int length) {
-		int min=arr[0];
-//		다음시간 퀴즈!!
-		for(int count=1; count<length; count++) {
-		if( min > arr[count])
-			min=	arr[count];
+	int findMin(int* arr, int length, int start) {
+		int min=arr[start];
+    //다음시간 퀴즈!!
+		for(int i=start +1; i<length; i++) {
+		if( min > arr[i])
+		{min=	arr[i];}
 		}
 		return min;
 	}
-		//함수: swapElements()
+    //함수: swapElements()
 	//입력: 배열, 두개의 인덱스
 	//출력: 없음
 	//부수효과: 배열의 두 인덱스 요소가 바뀐다.
 	void swapElement(int* arr, int i, int j) {
-		int temp;
-		temp = arr[i];
+		int temp = arr[i];
 		arr[i] = arr[j];
 		arr[j] = temp;
 	}
@@ -55,7 +54,10 @@
 	//출력: 없음
 	//부수효과: 배열이 정렬
 	void selectionSort(int* arr, int length) {
-    int i=0; int j=0;
+    for(int i = 0; i<length-1; i++){
+		int minindex = findMinIndex(arr, length, i);
+		swapElement(arr, minindex, i);
+	}
 	}
 		
 		
@@ -66,11 +68,13 @@ int main() {
 
 
 	int a[] = {30, 35, 27, 15, 40};
-	printf("minimum value of the array is %d\n", findMin(a, 5));
-		printf("The index of the minimum value is %d\n", findMinIndex(a, 5));
-	swapElement(a, 0, 3);
-		printerArray(a, 5);
-		
+	printf("minimum value of the array is %d\n", findMin(a, 5, 0));
+		printf("The index of the minimum value is %d\n", findMinIndex(a, 5, 0));
 	
+
+	
+	printerArray(a, 5);
+		selectionSort(a, 5);
+		printerArray(a, 5);
 	return 0;
 }
